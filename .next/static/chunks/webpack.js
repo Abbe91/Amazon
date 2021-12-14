417d5c6471557f4a6d42aae9e831fa30f220b7fb
Abdullah <Abdullah@zenio.se>
2021-12-14 16:18:02 +0100

    Added next

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "caa97a25a94c82246408"; }
+/******/ 		__webpack_require__.h = function() { return "1621eae3ed0143554f30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
