7603361900010e865794a2c38baeb3e3703998ae
Abdullah <Abdullah@zenio.se>
2021-12-17 18:04:10 +0100

    Create Menu bar and fetch products.

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,8 +49,22 @@ function Header() {
           </div>
         </div>
       </div>
-      {/* Bottom nav */}
-      <div></div>
+      {/* Bottom nav 1:09:04 */}
+      <div className="flex items-center space-x-3 p-2 pl-6 bg-amazon_blue-light text-white text-sm">
+        <p className="link flex items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Elecotranics</p>
+        <p className="link hidden lg:inline-flex">Food & Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health & Personal Care</p>
+      </div>
     </header>
   )
 }
